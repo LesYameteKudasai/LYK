6da0fb3156b7e87db373acf263f7158862751a96
Dynaveo <damien.michel13@sfr.fr>
2016-12-01 19:43:19 +0100

    Ajout de index.php

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Première page des Yamete Kudasai ~</title>
+</head>
+<body>
+	Page d'accueil des Yamete Kudasai pour pouvoir manger des Pizza ! <br /> 
+	<a href="/yamete-kudasai.png" /> <br />
+	<a href="/pizza.jpg" /> <br />
+
+</body>
+</html>
