f11bc67e0d17e0cfeffb5f8bfcca8a51c3fe7746
khilis <sundownerlyris@gmail.com>
2016-12-02 00:49:27 +0100

    test 2 eme branche

---
+++ plancamps.php
@@ -0,0 +1,20 @@
+<!Doctype HTML>
+<html>
+	<?php include_once "head.php"; ?>
+	<body class="navbar-fixed-top">
+	<?php include_once "header.php"; ?>
+
+	<table>
+	
+		<tr>
+		<td>
+			<img src="calaisOK.pnj" alt="" />
+		</td>
+		</tr>
+	
+	</table>
+
+
+	<?php include_once "footer.php"; ?>
+	</body>
+</html>
