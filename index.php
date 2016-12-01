3afe4975ef9f67df115b5517fef2206bb8598a23
Jonathan <jondu60@yahoo.fr>
2016-12-02 00:00:37 +0100

    Mise en place de l'index v1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Première page des Yamete Kudasai ~</title>
+</head>
+<body>
+	<h2>Coucou Benji ! On a la page d'accueil pour pouvoir manger des Pizza ! </h2><br /> 
+	<img src="pizza.jpg" width=60% height=60% /> <br />
+
+</body>
+</html>
