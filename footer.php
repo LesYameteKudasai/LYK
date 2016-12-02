c2cdf9ea8d4736eb2b1661da4c050eb5022ad915
KevFleu <fleurit.kevin@gmail.com>
2016-12-02 05:10:48 +0100

    Add files via upload
    Ajout head.php + MaJ footer.php + 2 fichiers css

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="container">
+	<p>&copy; 2016 Les Yamete Kumasai, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"> </script>
