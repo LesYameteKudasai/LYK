5ed0db6038f366c2f006ee067dd4743a4488167a
Jonathan <jondu60@yahoo.fr>
2016-12-02 06:50:07 +0100

    Gros commit

--- header.php
+++ header.php
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-<!-- <link href="header.css" rel="stylesheet"> -->
-
-<header>
-	<img src="LOGOSITE1.png" alt="Logo" title="Logo"/>
-	<h1>Le Bon Camp</h1>
-	<?php include_once "menu.php";?>
-</header>
-=======
-
-
 <header>
 	<img src="LOGOSITE1.png" width="150px" height="auto" alt="Logo" title="Logo"/>
 	<h1>Le Bon Camp</h1>
 	<?php include_once "menu.php";?>
 </header>
->>>>>>> Kevin
