49e3cf06608c1a9114574d99c6a57ec65868e829
Amako <amako@amako.com>
2016-12-02 01:09:46 +0100

    Ajout footer (php et css)

---
+++ footer.php
@@ -0,0 +1,7 @@
+<!--<link href="footer.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">-->
+<footer class="footer">
+  <div class="container">
+	<p class="text-muted">Contenu</p>
+  </div>
+</footer>
