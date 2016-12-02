38f51038b4f29d7420e98b16c3a2b70c91278846
itsizoh <tehizoh@gmail.com>
2016-12-02 04:13:07 +0100

    Add files via upload
    Ajout des champs spécifiques aux réfugiés et aux bénévoles dans le formulaire

--- inscription.php
+++ inscription.php
@@ -9,6 +9,8 @@ if(isset($_POST['forminscription'])) {
    $mail2 = htmlspecialchars($_POST['mail2']);
    $mdp = sha1($_POST['mdp']);
    $mdp2 = sha1($_POST['mdp2']);
+   $nationnalite = htmlspecialchars($_POST['nationnalite']);
+   $idcamp = $_POST['idcamp'];
    if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
       $pseudolength = strlen($pseudo);
       if($pseudolength <= 255) {
@@ -23,14 +25,19 @@ if(isset($_POST['forminscription'])) {
                      {
                         if(isset($_GET('benevole')))
                         {
-                           $insertmbr = $bdd->prepare("INSERT INTO benevole(nom, prenom, pseudo, mail, motdepasse) VALUES(?, ?, ?, ?, ?)");
-                           $insertmbr->execute(array($nom, $prenom, $pseudo, $mail, $mdp));
+                           $metier = $_POST['metier'];
+                           $insertmbr = $bdd->prepare("INSERT INTO benevole(nom, prenom, pseudo, mail, motdepasse, nationnalite, idcamp, metier) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+                           $insertmbr->execute(array($nom, $prenom, $pseudo, $mail, $mdp, $idcamp, $metier));
                            $erreur = "Votre compte bénévole a bien été créé ! Vous pouvez maintenant vous connecter !";
                         }
                         else
                         {
-                           $insertmbr = $bdd->prepare("INSERT INTO refugie(nom, prenom, pseudo, mail, motdepasse) VALUES(?, ?, ?, ?, ?)");
-                           $insertmbr->execute(array($nom, $prenom, $pseudo, $mail, $mdp));
+                           $date_naiss = $_POST['date_naiss'];
+                           $sexe = $_POST['sexe'];
+                           $idetat = $_POST['idetat'];
+                           $date_arrivee = $_POST['date_arrivee'];
+                           $insertmbr = $bdd->prepare("INSERT INTO refugie(nom, prenom, pseudo, mail, motdepasse, nationnalite, idcamp, date_naiss, sexe, idetat, date_arrivee) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                           $insertmbr->execute(array($nom, $prenom, $pseudo, $mail, $mdp, $idcamp, $date_naiss, $sexe, $idetat, $date_arrivee));
                            $erreur = "Votre compte réfugié a bien été créé ! Vous pouvez maintenant vous connecter !";
                         }
                      }
@@ -124,6 +131,75 @@ if(isset($_POST['forminscription'])) {
                      <input type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" />
                   </td>
                </tr>
+               <tr>
+                  <td align="right">
+                     <label for="nationnalite">Nationnalité :</label>
+                  </td>
+                  <td>
+                     <input type="text" placeholder="Votre Nationnalité" id="nationnalite" name="nationnalite" />
+                  </td>
+               </tr>
+               <tr>
+                  <td align="right">
+                     <label for="idcamp">id camp :</label>
+                  </td>
+                  <td>
+                     <input type="number" id="idcamp" name="idcamp" step="1" value="1" min="1" max="3" />
+                  </td>
+               </tr>
+               <?php 
+                  if(isset($_GET['benevole']))
+                  {
+                     echo "
+                     <tr>
+                        <td align="right">
+                           <label for="metier">Métier :</label>
+                        </td>
+                        <td>
+                           <input type="text" placeholder="Rentrez votre métier" id="metier" name="metier" />
+                        </td>
+                     </tr>
+                     ";
+                  }
+
+                  if(isset($_GET['refugie']))
+                  {
+                     echo "
+                     <tr>
+                        <td align="right">
+                           <label for="date_naiss">Date de naissance :</label>
+                        </td>
+                        <td>
+                           <input type="date" id="date_naiss" name="date_naiss" />
+                        </td>
+                     </tr>
+                     <tr>
+                        <td align="right">
+                           <label for="sexe">Sexe :</label>
+                        </td>
+                        <td>
+                           <input type="number" id="sexe" name="sexe" step="1" value="0" min="0" max="1" />
+                        </td>
+                     </tr>
+                     <tr>
+                        <td align="right">
+                           <label for="sexe">id état :</label>
+                        </td>
+                        <td>
+                           <input type="number" id="idetat" name="idetat" step="1" value="1" min="1" max="6" />
+                        </td>
+                     </tr>
+                     <tr>
+                        <td align="right">
+                           <label for="date_arrivee">Date d'arrivée :</label>
+                        </td>
+                        <td>
+                           <input type="date" id="date_arrivee" name="date_arrivee" />
+                        </td>
+                     </tr>
+                     ";
+                  }
+               ?>
                <tr>
                   <td></td>
                   <td align="center">
