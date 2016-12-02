236a6c5a5ea6d120dca9c362629c1778ab4c4338
LesYameteKudasai <LesYameteKudasai@users.noreply.github.com>
2016-12-02 02:40:25 +0100

    Delete header.php
    Supp header.php - erreur merge

--- header.php
+++
@@ -1,7 +0,0 @@
-<!-- <link href="header.css" rel="stylesheet"> -->
-
-<header>
-	<img src="LOGOSITE1.png" alt="Logo" title="Logo"/>
-	<h1>Le Bon Camp</h1>
-	<?php include_once "menu.php";?>
-</header>
